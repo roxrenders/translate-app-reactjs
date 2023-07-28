339baef41cbbc17493038f05b28a5dbedaf6daca
rox <raunaksahu55@gmail.com>
2023-07-28 13:09:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+
 
 function App() {
+   const[options, setOptions] = useState([]);
+   const[to, setTo] = useState('en');
+   const[from, setFrom] = useState('en');
+   const[input, setInput] = useState('');
+   const[output, setOutput] = useState('');
+
+  //  curl -X 'POST' \
+  //  'https://libretranslate.com/detect' \
+  //  -H 'accept: application/json' \
+  //  -H 'Content-Type: application/x-www-form-urlencoded' \
+  //  -d 'q=Hello%20world!&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+
+ const translate = ()=>{
+  const params = new URLSearchParams();
+  params.append('q', input);
+  params.append('source', from);
+  params.append('target',to);
+  params.append('api_key','xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+  axios.post('https://libretranslate.de/translate',params,
+  {headers: { 
+    'accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+   },
+  }).then(res=>{
+    console.log(res.data )
+    setOutput(res.data.translatedText)
+  })
+ }
+
+ useEffect(() => {
+  axios.get('https://libretranslate.com/languages',
+  {headers: { 'accept': 'application/json' }
+  }).then(res => {
+    setOptions(res.data);
+  })
+},{});
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      Form({from}):
+      <select onClick={e=>setFrom(e.target.value)} >
+       {options.map(opt=><option key={opt.code} value={opt.code}>{opt.name}</option>)
+       } 
+      </select>
+      To({to}):
+      <select onClick={e=>setTo(e.target.value)}>
+      {options.map(opt=><option key={opt.code} value={opt.code}>{opt.name}</option>)
+       }
+      </select>
+     </div>
+     <div>
+     <textarea onInput={e=>setInput(e.target.value)} id="" cols="50" rows="10"  ></textarea>
+     </div>
+     <div>
+      <textarea value={output} cols="50" rows="10"></textarea>
+    </div>
+    <div>
+      <button onClick={e=>translate()}>Translate</button>
+    </div>
     </div>
   );
 }
